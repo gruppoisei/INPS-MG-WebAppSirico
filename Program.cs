0d767a5c049ccad24e6909bfc385d91703929516
Alessandro <deangelisalessandro2000@gmail.com>
2024-12-02 10:57:55 +0100

    Aggiunto reference pacchetto titillium web per soluzione angular. Impostato che all'avvio della soluzione l'avvio dell'index.html.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSpaStaticFiles(configuration =>
+{
+    configuration.RootPath = "ClientApp/dist"; // Percorso ai file Angular compilati
+});
 
 var app = builder.Build();
 
@@ -15,13 +22,26 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseSpaStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+
+    // Configura per servire la build prodotta (dist)
+    spa.Options.DefaultPageStaticFileOptions = new StaticFileOptions
+    {
+        FileProvider = new PhysicalFileProvider(
+            Path.Combine(Directory.GetCurrentDirectory(), "ClientApp", "dist")) // Usa il dist
+    };
+});
 
 app.Run();
